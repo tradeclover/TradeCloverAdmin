3d483e49aea7dab801551007e7bf509f24d51aae
gosavia <ashish.gosavi525@gmail.com>
2026-05-18 11:18:12 +0530

    API URL changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //const API_BASE_URL = 'https://api-tradeclover.techsperia.in/api';
 //local API
-const API_BASE_URL = 'http://192.168.1.35:8000/api';
+const API_BASE_URL = 'http://192.168.1.37:8000/api';
 const REFRESH_TOKEN_URL = `${API_BASE_URL}/auth/refresh/`;
 
 const redirectToLogin = () => {
